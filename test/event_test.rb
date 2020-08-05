64af02e99e9b65ededaff331c010fa5838b10973
Shaun James <ShaunDaneJames@Gmail.com>
2020-08-05 09:30:05 -0600

    Adds and passes existence with attributes

---
+++ test/event_test.rb
@@ -0,0 +1,14 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/item'
+require './lib/food_truck'
+require './lib/event'
+
+class EventTest < Minitest::Test
+
+  def test_it_exists_with_attributes
+    event = Event.new("South Pearl Street Farmers Market")
+    assert_instance_of Event, event
+  end
+  
+end
