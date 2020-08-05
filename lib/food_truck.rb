b031adaca9c7aeaa0ca5c5e6296f962191c4440c
Shaun James <ShaunDaneJames@Gmail.com>
2020-08-05 09:15:23 -0600

    Adds and passes stock item

--- lib/food_truck.rb
+++ lib/food_truck.rb
@@ -1,7 +1,16 @@
 class FoodTruck
-  attr_reader :name
+  attr_reader :name,
+              :inventory
   def initialize(name)
     @name = name
+    @inventory = {}
   end
 
+  def check_stock(item)
+    @inventory.fetch(item, 0)
+  end
+
+  def stock(item, quantity)
+    @inventory[item] = quantity
+  end
 end
--- test/food_truck_test.rb
+++ test/food_truck_test.rb
@@ -11,5 +11,20 @@ class FoodTruckTest < Minitest::Test
     assert_equal "Rocky Mountain Pies", food_truck.name
   end
 
+  def test_starts_with_no_inventory
+    food_truck = FoodTruck.new("Rocky Mountain Pies")
+    assert_equal Hash.new, food_truck.inventory
+  end
 
+  def test_can_check_stock_return_0_if_not_found
+    food_truck = FoodTruck.new("Rocky Mountain Pies")
+    item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
+
+    assert_equal 0, food_truck.check_stock(item1)
+
+    food_truck.stock(item1, 30)
+    expected = {item1 => 30}
+    assert_equal expected, food_truck.inventory
+    assert_equal 30, food_truck.check_stock(item1)
+  end
 end
