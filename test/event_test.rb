091a90bcaa166694aa8e4ff38fa9782f1c0f821a
Shaun James <ShaunDaneJames@Gmail.com>
2020-08-05 09:31:41 -0600

    Adds and passes starts with no food trucks

--- test/event_test.rb
+++ test/event_test.rb
@@ -9,6 +9,11 @@ class EventTest < Minitest::Test
   def test_it_exists_with_attributes
     event = Event.new("South Pearl Street Farmers Market")
     assert_instance_of Event, event
+    assert_equal "South Pearl Street Farmers Market", event.name
   end
-  
+
+  def test_starts_with_no_food_trucks
+    event = Event.new("South Pearl Street Farmers Market")
+    assert_equal [], event.food_trucks
+  end
 end
