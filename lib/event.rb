d0e73b52ab739c1f39809bf70bb4fcb45df1c83e
Shaun James <ShaunDaneJames@Gmail.com>
2020-08-05 10:43:30 -0600

    Adds total inventory test

--- lib/event.rb
+++ lib/event.rb
@@ -19,11 +19,13 @@ class Event
   def food_trucks_that_sell(item)
     available_at = []
     @food_trucks.map do |food_truck|
-      # require "pry"; binding.pry
       if food_truck.check_stock(item) != 0
         available_at << food_truck
       end
     end
     available_at
   end
+
+  def total_inventory
+  end 
 end
