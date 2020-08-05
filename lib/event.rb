e39255b805f761e671b3df281733c4cab0862a56
Shaun James <ShaunDaneJames@Gmail.com>
2020-08-05 09:58:16 -0600

    Adds and passes food trucks that sell item

--- lib/event.rb
+++ lib/event.rb
@@ -15,4 +15,15 @@ class Event
       food_truck.name
     end
   end
+
+  def food_trucks_that_sell(item)
+    available_at = []
+    @food_trucks.map do |food_truck|
+      # require "pry"; binding.pry
+      if food_truck.check_stock(item) != 0
+        available_at << food_truck
+      end
+    end
+    available_at
+  end
 end
